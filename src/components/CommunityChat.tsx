ed4a2db93968f1f13ff67285fe1637442322c7a9
BDanielo <daniel@armieux.fr>
2025-02-10 14:36:47 +0100

    fix(chat) : fix scroll now default to off

--- src/components/CommunityChat.tsx
+++ src/components/CommunityChat.tsx
@@ -125,7 +125,7 @@ export default function CommunityChat() {
         />
       </div>
 
-      <MessageList messages={messages} shoulScroll={true} />
+      <MessageList messages={messages} shoulScroll={false} />
 
       <MessageInput
         newMessage={newMessage}
